c33c22b1f03962cf946cd775905bceee3164b52e
seanpeng12 <seanpeng12@gmail.com>
2020-03-02 16:49:27 +0800

    test.blade.php進行測試

--- resources/views/frontend_sna/test.blade.php
+++ resources/views/frontend_sna/test.blade.php
@@ -27,6 +27,7 @@
             appId: "1:640892044634:web:3c3c94c360528786d31f63",
             measurementId: "G-D1PL8FR9EF"
         };
+
     </script>
 </head>
 
@@ -89,6 +90,7 @@
                 }]
             });
         });
+
     </script>
 
 
@@ -97,121 +99,188 @@
     {{-- 原本的ajax script位置 --}}
     <script>
         // Your web app's Firebase configuration
-            var firebaseConfig = {
-                apiKey: "AIzaSyB2XfJ2AwM1FiciKO23zexRNIqTj284Dtw",
-                authDomain: "laravel-crud-seanpeng12.firebaseapp.com",
-                databaseURL: "https://laravel-crud-seanpeng12.firebaseio.com",
-                projectId: "laravel-crud-seanpeng12",
-                storageBucket: "laravel-crud-seanpeng12.appspot.com",
-                messagingSenderId: "44144922525",
-                appId: "1:44144922525:web:cb45a36fe77e7509ca6974",
-                measurementId: "G-HES2RGKZYZ"
-            };
-            // Initialize Firebase
-            firebase.initializeApp(firebaseConfig);
-            // firebase.analytics();
+        var firebaseConfig = {
+            apiKey: "AIzaSyB2XfJ2AwM1FiciKO23zexRNIqTj284Dtw",
+            authDomain: "laravel-crud-seanpeng12.firebaseapp.com",
+            databaseURL: "https://laravel-crud-seanpeng12.firebaseio.com",
+            projectId: "laravel-crud-seanpeng12",
+            storageBucket: "laravel-crud-seanpeng12.appspot.com",
+            messagingSenderId: "44144922525",
+            appId: "1:44144922525:web:cb45a36fe77e7509ca6974",
+            measurementId: "G-HES2RGKZYZ"
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+        // firebase.analytics();
 
 
-            const db = firebase.firestore();
-            const settings = {/* your settings... */ timestampsInSnapshots: true};
-            db.settings(settings);
+        const db = firebase.firestore();
+        const settings = {
+            /* your settings... */
+            timestampsInSnapshots: true
+        };
+        db.settings(settings);
 
-            // var db = firebase.firestore();
+        // var db = firebase.firestore();
 
 
-            function test123() {
-                db.collection("喜好項目").doc("喜歡的地點").set({
-                    name: "行天宮",
-                    date: "202002",
-                    desctiption: "本劇改編自阿瑟·柯南·道爾爵士家喻戶曉的推理小說，一位脾氣古怪的大偵探在現代倫敦的街頭悄悄巡行，四處搜尋線索。",
-                    actors: ["班尼迪克·康柏拜區", "馬丁·費曼"]
-                });
-                console.log("傳資料");
-            }
-            function new_getdata() {
-                var index = 0;
-                var lastIndex = 0;
-                var htmls = [];
+        function test123() {
+            db.collection("喜好項目").doc("喜歡的地點").set({
+                name: "行天宮",
+                date: "202002",
+                desctiption: "本劇改編自阿瑟·柯南·道爾爵士家喻戶曉的推理小說，一位脾氣古怪的大偵探在現代倫敦的街頭悄悄巡行，四處搜尋線索。",
+                actors: ["班尼迪克·康柏拜區", "馬丁·費曼"]
+            });
+            console.log("傳資料");
+        }
 
-                var docRef = db.collection("喜歡的地點");
-                // 根據date排序
+        function new_getdata() {
+            var index = 0;
+            var lastIndex = 0;
+            var htmls = [];
+            var htmls_2 = [];
+            var doc = [];
+
+            var docRef = db.collection("喜歡的地點");
+            // 根據date排序
+
+
+            if (docRef == false) {
                 docRef = docRef.orderBy("date");
-                docRef.get().then(querySnapshot => {
-                    querySnapshot.forEach(function(doc, index) {
-                        if (doc.exists) {
-                            console.log(doc.id, doc.data());
 
-                            htmls.push('<tr>\
-                                <td>'+ doc.data().date +'</td>\
-                                <td>'+ doc.data().desctiption +'</td>\
-                                <td>'+ doc.data().city +'</td>\
-                                <td>'+ doc.data().catagory_1 +'</td>\
-                                <td>'+ doc.data().catagory_2 +'</td>\
+                docRef.get().then(function (querySnapshot) {
+                        querySnapshot.forEach(function (doc) {
+                            // doc.data() is never undefined for query doc snapshots
+                            console.log(doc.id, " => ", doc.data().desctiption);
+                            if (doc.exists && name !== undefined) {
+                                // console.log(doc.id, doc.data());
+
+                                htmls.push('<tr>\
+                                <td>' + doc.data().date + '</td>\
+                                <td>' + doc.data().desctiption + '</td>\
+                                <td>' + doc.data().city + '</td>\
+                                <td>' + doc.data().catagory_1 + '</td>\
+                                <td>' + doc.data().catagory_2 + '</td>\
                                 <td><button>修改最愛</button>\
-                                    <button id='+ doc.id +' onclick="delete_button()">刪除最愛</button>\
+                                    <button id=' + doc.id + ' onclick="delete_button()">刪除最愛</button>\
                             </tr>');
-                            lastIndex = index;
-                            $('#tbody').html(htmls);
-                        } else {
-                            console.log("找不到文件");
-                        }
+                                lastIndex = index;
+                                $('#tbody').html(htmls);
+                            };
+                        });
+                    })
+                    .catch(function (error) {
+                        console.log("Error getting documents: ", error);
                     });
-                });
-            }
-            var id_delete = "";
-            function delete_button() {
+            } else {
+                htmls_2.push('<tr>\
+                                <td>' + "無資料" + '</td>\
+                                <td>' + "無資料" + '</td>\
+                                <td>' + "無資料" + '</td>\
+                                <td>' + "無資料" + '</td>\
+                                <td>' + "無資料" + '</td>\
+                                <td><button>修改最愛</button>\
+                                    <button id=' + "無資料" + ' onclick="delete_button()">刪除最愛</button>\
+                            </tr>');
 
-                document.body.onclick = function(event){    //抓到id
-                    id_delete = event.target.id;
-                    console.log(id_delete+"正在刪除...");
+                $('#tbody').html(htmls_2);
+            };
 
-                    deletedata(id_delete);
+
+
+            docRef.onSnapshot(querySnapshot => {
+                let changes = querySnapshot.docChanges();
+                for (let change of changes) {
+                    console.log(`資料動態：A document was ${change.type}.`);
                 }
+            });
 
+            // docRef.get().then(querySnapshot => {
+            //     querySnapshot.forEach(function (doc, index) {
+            //         // doc.data() is never undefined for query doc snapshots
+            //         if (doc.exists) {
+            //             console.log(doc.id, doc.data());
+
+            //             htmls.push('<tr>\
+            //                     <td>' + doc.data().date + '</td>\
+            //                     <td>' + doc.data().desctiption + '</td>\
+            //                     <td>' + doc.data().city + '</td>\
+            //                     <td>' + doc.data().catagory_1 + '</td>\
+            //                     <td>' + doc.data().catagory_2 + '</td>\
+            //                     <td><button>修改最愛</button>\
+            //                         <button id=' + doc.id + ' onclick="delete_button()">刪除最愛</button>\
+            //                 </tr>');
+            //             lastIndex = index;
+            //             $('#tbody').html(htmls);
+            //             console.log("已經顯示!");
+            //         } else {
+            //             // console.log("找不到文件");
+            //             console.log("no file!!!!!");
+            //         }
+            //     });
+            // });
+        }
+        var id_delete = "";
+
+        function delete_button() {
+
+            document.body.onclick = function (event) { //抓到刪除 id
+                id_delete = event.target.id;
+                console.log("已刪除ID:" + id_delete);
+
+                deletedata(id_delete);
+
+                //重整頁面
+                new_getdata();
             }
+        }
 
-            function getdata() {
-                var docRef = db.collection("喜歡的地點");
-                // 網路
-                // var ref = db.collection('fruit');
-                // ref.get().then(querySnapshot => {
-                //     querySnapshot.forEach(doc => {
-                //         console.log(doc.id, doc.data());
-                //     });
-                // });
+        function deletedata(id_delete) {
+            var id = id_delete;
+            //doc文件內資料清空
+            db.collection("喜歡的地點").doc(id).set({});
+            //doc整個刪除
+            db.collection("喜歡的地點").doc(id).delete();
+        }
 
-                docRef.get().then(querySnapshot => {
-                    querySnapshot.forEach(function(doc) {
-                        if (doc.exists) {
-                            console.log(doc.id, doc.data());
-                        } else {
-                            console.log("找不到文件");
-                        }
-                    });
+
+        function getdata() {
+            var docRef = db.collection("喜歡的地點");
+            // 網路
+            // var ref = db.collection('fruit');
+            // ref.get().then(querySnapshot => {
+            //     querySnapshot.forEach(doc => {
+            //         console.log(doc.id, doc.data());
+            //     });
+            // });
+
+            docRef.get().then(querySnapshot => {
+                querySnapshot.forEach(function (doc) {
+                    if (doc.exists) {
+                        console.log(doc.id, doc.data());
+                    } else {
+                        console.log("找不到文件");
+                    }
                 });
-                // 原本
-                // docRef.get().then(function(doc) {
-                //         if (doc.exists) {
-                //             console.log(doc.data());
-                //         } else {
-                //             console.log("找不到文件");
-                //         }
-                //     }).catch(function(error) {
-                //     console.log("提取文件時出錯:", error);
-                // });
-            }
+            });
+            // 原本
+            // docRef.get().then(function(doc) {
+            //         if (doc.exists) {
+            //             console.log(doc.data());
+            //         } else {
+            //             console.log("找不到文件");
+            //         }
+            //     }).catch(function(error) {
+            //     console.log("提取文件時出錯:", error);
+            // });
+        }
 
-            function updatedata() {
-                db.collection("喜好項目").doc("新世紀福爾摩斯").set({
-                    date: "1000",
-                });
-            }
+        function updatedata() {
+            db.collection("喜好項目").doc("新世紀福爾摩斯").set({
+                date: "1000",
+            });
+        }
 
-            function deletedata(id_delete) {
-                var id = id_delete;
-                db.collection("喜歡的地點").doc(id).set({
-                });
-            }
     </script>
 
 
