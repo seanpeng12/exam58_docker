daee7abc60bd30687d0cf14a2724c9463c16297f
possess0602 <48153269+possess0602@users.noreply.github.com>
2020-03-30 16:29:53 +0800

    建立好了quasar

---
+++ quasarapp/src/pages/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page class="flex flex-center">
+    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
+    <input v-model="message" />
+    <p>{{ message }}</p>
+    <button @click="clear">clear</button>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageIndex",
+  data() {
+    return {
+      message: "I love vue.js++++++++++++++++++++"
+    };
+  },
+  methods: {
+    clear() {
+      this.message = "";
+    }
+  }
+};
+</script>
---
+++ quasarapp/src/pages/class.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-page padding>
+    <q-card>
+      <q-tabs
+        v-model="tab"
+        dense
+        class="text-grey"
+        active-color="primary"
+        indicator-color="primary"
+        align="justify"
+        narrow-indicator
+      >
+        <q-tab name="mails" label="Mails" />
+        <q-tab name="alarms" label="Alarms" />
+        <q-tab name="movies" label="Movies" />
+      </q-tabs>
+
+      <q-separator />
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="mails">
+          <div class="text-h6">Mails</div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </q-tab-panel>
+
+        <q-tab-panel name="alarms">
+          <div class="text-h6">Alarms</div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </q-tab-panel>
+
+        <q-tab-panel name="movies">
+          <div class="text-h6">Movies</div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </q-tab-panel>
+      </q-tab-panels>
+    </q-card>
+  </q-page>
+</template>
+<script>
+// import task from "components/task.vue";
+export default {
+  data() {
+    return {
+      tab: "mails"
+    };
+  }
+};
+</script>
